2d0b01e9799d29bd7e8609086cb82fc18adec95a
Robbe Van Petegem <git@robbevp.be>
2024-08-11 22:36:04 +0200

    Fix API url

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,12 @@
 import { createApiClient } from "@accentor/api-client-js";
 
 let url;
-if (import.meta.env.VUE_APP_API_URL !== undefined) {
-  url = import.meta.env.VUE_APP_API_URL;
+if (import.meta.env.VITE_API_URL !== undefined) {
+  url = import.meta.env.VITE_API_URL;
 } else if (import.meta.env.NODE_ENV === "production") {
   url = "/api";
 } else {
-  // url = "http://localhost:3000/api";
-  url = "https://accentor.vanpetegem.me/api";
+  url = "http://localhost:3000/api";
 }
 
 export const baseURL = url;
